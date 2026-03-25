c037d8d8a108d58ad43903b878ae7426baa006cf
curcumanLM <ssingh@institutmvm.cat>
2026-03-25 19:25:10 +0000

    Entrega final

--- examen.py
+++ examen.py
@@ -1,22 +1,25 @@
 #Generador de noms incorrectes per Timothée Chalamet
 
 # Posa aquí qualsevol import que necessitis com rand, math, time...
+import random
 
 
 def obtenir_nom():
-    # Llista de noms incorrectes
+    
     noms = ["Timotei", "Timonel", "Timbaler", "Tennebaum", "TaoPaiPai", "Teruel", "Tirolés", "Traginer", "Tourmalet"]
-
-    # Llista de cognoms incorrectes
     cognoms = ["Chandalet", "Camembert", "Sabadell", "Chevrolet", "Caganer", "Bechamel", "Casteller", "Churumbel", "Cafeaulait", "Crivillé", "Charmander"]
 
+    nom = random.choice(noms)
+    cognom = random.choice(cognoms)
 
-    print("PENDENT: obtenir_nom")
+    return nom + "" + cognom
     # Aquí has de construir un nom amb un nomb aleatori i un cognom aleatori de les llistes
     # retornar el nom construït
 
 def afegir_nom(llista):
-    print("PENDENT: afegir_nom")
+    nou_nom = obtenir_nom()
+    llista.append(nou_nom)
+    print(f"S'ha afegit un nou nom:"{nou_nom})
     # Hem d'obtenir un nom aleatori, afegir-lo a la llista i mostrar per pantalla que hem afegit aquest nom
 
 def llistar_noms(llista):
@@ -29,11 +32,20 @@ def ordenar_noms(llista):
     # Un cop ordenada la llista, llistem tots els noms
 
 def mostrar_menu():
-    print("PENDENT: mostrar_menu")
+    print("----Menu----")
+    print("(A)--Afegir-Nom")
+    print("(L)--Llistar-Noms")
+    print("(O)-Ordenar-Noms")
+    print("(F)-Finalitzar")
     # Hem de mostrar el menú que ens demanen a l'enunciat
 
+
 def demanar_opcio():
-    print("PENDENT: demanar_opcio")
+    opcions = ["A", "L", "O", "F"]
+    while True:
+        opcio = input("Selecciona una opció").lower()
+    
+    if opcio in opcions
     # Hem de demanar a l'usuari una de les opcions del menú
     # Si ens introdueix un valor incorrecte hem de tornar a mostrar el menú i tornar a demanar opció
     # Si ens introdueix la lletra correcta en minúscula, la donarem per bona
@@ -41,6 +53,17 @@ def demanar_opcio():
 
 def gestionar_opcio(opcio, llista):
     print("PENDENT: gestionar_opcio")
+    match opcio:
+        case "A":
+            afegir_nom
+        case "L":
+            llistar_noms
+        case "O":
+            ordenar_noms
+        case "F":
+
+
+
     # En funció de l'opció escollida per l'usuari, haurem de cridar a les funcions adients per fer el que ens demanen
     # Heu de fer servir `match`
     # Si no ho sabeu fer amb `match` podeu utilitzar altres estructures condicionals però no obtindreu tota la puntuació    
